4348e3e02313b19904152cbb588ae6987cb2adc1
GustavoCMatias <gustavocmatias@gmail.com>
2022-10-27 20:39:31 -0300

    Entrada e conexão configurados

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let nome;
+Botarnome();
 
+function Botarnome() {
+    nome = prompt("Qual o seu nome?");
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', { name: nome })
+    promessa.catch(Botarnome);
+    setInterval(ManterConexao, 4500);
+}
+
+function ManterConexao() {
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', { name: nome })
+    promessa.then(bom);
+}
 
 
